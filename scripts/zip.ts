8e0fcae7fce3e3bb3153454487077726abf76b57
mnao305 <naomasa305@gmail.com>
2020-04-29 03:03:54 +0900

    build: 出力ファイル名の変更

--- scripts/zip.ts
+++ scripts/zip.ts
@@ -1,7 +1,7 @@
 import archiver from 'archiver'
 import fs from 'fs'
 import path from 'path'
-import { name, version } from '../package.json'
+// import { name, version } from '../package.json'
 
 /**
  * 吐き出されるzipファイルを置くディレクトリを作る
@@ -32,7 +32,7 @@ const makeZip = (src: string, output: string, zipFilename: string): void => {
 const main = (): void => {
   const SRC_DIR = path.join(__dirname, '../dist')
   const OUTPUT_DIR = path.join(__dirname, '../dist_zip')
-  const OUTPUT_FILE = `${name}_v${version}.zip`
+  const OUTPUT_FILE = 'file.zip'
 
   createDir(OUTPUT_DIR)
   makeZip(SRC_DIR, OUTPUT_DIR, OUTPUT_FILE)
